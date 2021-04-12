683ff5d34f76f84f21b4875311091dda5a3283fc
ssavadeumuhl <71304261+ssavadeumuhl@users.noreply.github.com>
2021-04-12 15:59:48 +0900

    Add files via upload

---
+++ js/js_sidebox.js
@@ -0,0 +1,8 @@
+function sidebox(){
+var html = "";
+html += '<div class="side_box">';
+html += '<div id="side">';
+html += '<div class="inner"><br><br><br>製作途中<br><br><br><br></div>';
+html += '</div></div>';
+document.write(html);
+}
